418faba6329532e1118b769cfa8852fc5ad41538
Alamgir Farouk <afarouk2@hotmail.com>
2017-01-07 01:34:52 -0800

    Site update

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
 <head>
   <!-- Mobile Specific Metas
   ================================================== -->
+    <link rel="apple-touch-startup-image" href="img/launch.png">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
     <meta name="description" content="">
     <meta name="author" content="Orinoco Inc.">
 
@@ -195,7 +199,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h2>How it works</h2>
-                    <p>Pick the features you want from our comprehensive list to create your link, send it to your fans and customer. </p>
+                    <p>Pick the features you want from our comprehensive list to create your link, send it to your fans and customers. </p>
                     <hr class="colored">
                 </div>
                 <img  class="img-responsive center-block" src="img/backgrounds/bg-platform.png">  </img>
